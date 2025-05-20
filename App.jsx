c5d3e2923b8f0c90a51bbd7736d2a01ba94da058
Shadowprofessor <sachitsharma802@gmail.com>
2025-05-21 02:30:26 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,24 @@
+// File: App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WelcomePage from './pages/WelcomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Dashboard from './pages/Dashboard';
+import BookDetail from './pages/BookDetail';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/book/:id" element={<BookDetail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
